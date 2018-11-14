7bede80afef453c57bc08ee58667c94b6ea42f5c
Immo Landwerth <immo@landwerth.net>
2018-11-14 08:41:37 -0800

    Mark error messages consistent

--- src/Minsk/CodeAnalysis/DiagnosticBag.cs
+++ src/Minsk/CodeAnalysis/DiagnosticBag.cs
@@ -46,13 +46,13 @@ namespace Minsk.CodeAnalysis
 
         public void ReportUndefinedUnaryOperator(TextSpan span, string operatorText, Type operandType)
         {
-            var message = $"Unary operator '{operatorText}' is not defined for type {operandType}.";
+            var message = $"Unary operator '{operatorText}' is not defined for type '{operandType}'.";
             Report(span, message);
         }
 
         public void ReportUndefinedBinaryOperator(TextSpan span, string operatorText, Type leftType, Type rightType)
         {
-            var message = $"Binary operator '{operatorText}' is not defined for types {leftType} and {rightType}.";
+            var message = $"Binary operator '{operatorText}' is not defined for types '{leftType}' and '{rightType}'.";
             Report(span, message);
         }
 

3c8bb6bcd92b138140036daf79dc7a303f4021f0
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-11-30 16:48:11 +0100

    Remove double negation in BoundConditionalGotoStatement usage

--- src/Minsk/CodeAnalysis/Binding/BoundConditionalGotoStatement.cs
+++ src/Minsk/CodeAnalysis/Binding/BoundConditionalGotoStatement.cs
@@ -2,16 +2,16 @@ namespace Minsk.CodeAnalysis.Binding
 {
     internal sealed class BoundConditionalGotoStatement : BoundStatement
     {
-        public BoundConditionalGotoStatement(LabelSymbol label, BoundExpression condition, bool jumpIfFalse = false)
+        public BoundConditionalGotoStatement(LabelSymbol label, BoundExpression condition, bool jumpIfTrue = true)
         {
             Label = label;
             Condition = condition;
-            JumpIfFalse = jumpIfFalse;
+            JumpIfTrue = jumpIfTrue;
         }
 
         public override BoundNodeKind Kind => BoundNodeKind.ConditionalGotoStatement;
         public LabelSymbol Label { get; }
         public BoundExpression Condition { get; }
-        public bool JumpIfFalse { get; }
+        public bool JumpIfTrue { get; }
     }
 }

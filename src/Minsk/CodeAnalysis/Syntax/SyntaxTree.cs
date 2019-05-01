3bc736b6868817147f0b73566f8c7b560a73e898
Immo Landwerth <immo@landwerth.net>
2019-05-01 08:51:59 -0700

    Add ability to get lexer diagnostics
    When parsing tokens only, we currently have no way to get access to the
    lexer's diagnostics. This adds an optional out parameter.
    
    The real fix, of course, would be to mirror what Roslyn is doing, which is
    exposing the diagnostics on the actual tokens, which makes the output neatly
    self-contained.

--- src/Minsk/CodeAnalysis/Syntax/SyntaxTree.cs
+++ src/Minsk/CodeAnalysis/Syntax/SyntaxTree.cs
@@ -31,23 +31,40 @@ namespace Minsk.CodeAnalysis.Syntax
             return new SyntaxTree(text);
         }
 
-        public static IEnumerable<SyntaxToken> ParseTokens(string text)
+        public static ImmutableArray<SyntaxToken> ParseTokens(string text)
         {
             var sourceText = SourceText.From(text);
             return ParseTokens(sourceText);
         }
 
-        public static IEnumerable<SyntaxToken> ParseTokens(SourceText text)
+        public static ImmutableArray<SyntaxToken> ParseTokens(string text, out ImmutableArray<Diagnostic> diagnostics)
         {
-            var lexer = new Lexer(text);
-            while (true)
-            {
-                var token = lexer.Lex();
-                if (token.Kind == SyntaxKind.EndOfFileToken)
-                    break;
+            var sourceText = SourceText.From(text);
+            return ParseTokens(sourceText, out diagnostics);
+        }
 
-                yield return token;
+        public static ImmutableArray<SyntaxToken> ParseTokens(SourceText text)
+        {
+            return ParseTokens(text, out _);
+        }
+
+        public static ImmutableArray<SyntaxToken> ParseTokens(SourceText text, out ImmutableArray<Diagnostic> diagnostics)
+        {
+            IEnumerable<SyntaxToken> LexTokens(Lexer lexer)
+            {
+                while (true)
+                {
+                    var token = lexer.Lex();
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+
+                    yield return token;
+                }
             }
+            var l = new Lexer(text);
+            var result = LexTokens(l).ToImmutableArray();
+            diagnostics = l.Diagnostics.ToImmutableArray();
+            return result;
         }
     }
 }

1d64d24556b0d063478077d40855cf86da563370
Immo Landwerth <immo@landwerth.net>
2019-05-01 08:15:55 -0700

    Move GetLastToken() to SyntaxNode

--- src/mc/MinskRepl.cs
+++ src/mc/MinskRepl.cs
@@ -66,19 +66,10 @@ namespace Minsk
             var syntaxTree = SyntaxTree.Parse(text);
 
             // Use Statement because we need to exclude the EndOfFileToken.
-            if (GetLastToken(syntaxTree.Root.Statement).IsMissing)
+            if (syntaxTree.Root.Statement.GetLastToken().IsMissing)
                 return false;
             
             return true;
-        }
-
-        private static SyntaxToken GetLastToken(SyntaxNode node)
-        {
-            if (node is SyntaxToken token)
-                return token;
-
-            // A syntax node should always contain at least 1 token.
-            return GetLastToken(node.GetChildren().Last());
         }
 
         protected override void EvaluateSubmission(string text)

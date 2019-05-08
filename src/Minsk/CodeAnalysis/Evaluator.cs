53d729f2bca858b510b6c8bc3ed9a200cbd62099
Immo Landwerth <immo@landwerth.net>
2019-05-08 09:36:44 -0700

    Add function for computing random numbers

--- src/Minsk/CodeAnalysis/Evaluator.cs
+++ src/Minsk/CodeAnalysis/Evaluator.cs
@@ -9,6 +9,7 @@ namespace Minsk.CodeAnalysis
     {
         private readonly BoundBlockStatement _root;
         private readonly Dictionary<VariableSymbol, object> _variables;
+        private Random _random;
 
         private object _lastValue;
 
@@ -202,6 +203,14 @@ namespace Minsk.CodeAnalysis
                 var message = (string)EvaluateExpression(node.Arguments[0]);
                 Console.WriteLine(message);
                 return null;
+            }
+            else if (node.Function == BuiltinFunctions.Rnd)
+            {
+                var max = (int)EvaluateExpression(node.Arguments[0]);
+                if (_random == null)
+                    _random = new Random();
+
+                return _random.Next(max);
             }
             else
             {
--- src/Minsk/CodeAnalysis/Symbols/BuiltinFunctions.cs
+++ src/Minsk/CodeAnalysis/Symbols/BuiltinFunctions.cs
@@ -10,6 +10,7 @@ namespace Minsk.CodeAnalysis.Symbols
     {
         public static readonly FunctionSymbol Print = new FunctionSymbol("print", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String)), TypeSymbol.Void);
         public static readonly FunctionSymbol Input = new FunctionSymbol("input", ImmutableArray<ParameterSymbol>.Empty, TypeSymbol.String);
+        public static readonly FunctionSymbol Rnd = new FunctionSymbol("rnd", ImmutableArray.Create(new ParameterSymbol("max", TypeSymbol.Int)), TypeSymbol.Int);
 
         internal static IEnumerable<FunctionSymbol> GetAll()
             => typeof(BuiltinFunctions).GetFields(BindingFlags.Public | BindingFlags.Static)

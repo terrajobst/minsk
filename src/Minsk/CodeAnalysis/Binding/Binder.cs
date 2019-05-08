ad32c7da012bf575c52ef29ebd583a6f6e7dc519
Immo Landwerth <immo@landwerth.net>
2019-05-08 09:33:43 -0700

    Add proper function lookup

--- src/Minsk/CodeAnalysis/Binding/Binder.cs
+++ src/Minsk/CodeAnalysis/Binding/Binder.cs
@@ -41,19 +41,29 @@ namespace Minsk.CodeAnalysis.Binding
                 previous = previous.Previous;
             }
 
-            BoundScope parent = null;
+            var parent = CreateRootScope();
 
             while (stack.Count > 0)
             {
                 previous = stack.Pop();
                 var scope = new BoundScope(parent);
                 foreach (var v in previous.Variables)
-                    scope.TryDeclare(v);
+                    scope.TryDeclareVariable(v);
 
                 parent = scope;
             }
 
             return parent;
+        }
+
+        private static BoundScope CreateRootScope()
+        {
+            var result = new BoundScope(null);
+
+            foreach (var f in BuiltinFunctions.GetAll())
+                result.TryDeclareFunction(f);
+
+            return result;
         }
 
         public DiagnosticBag Diagnostics => _diagnostics;
@@ -209,7 +219,7 @@ namespace Minsk.CodeAnalysis.Binding
                 return new BoundErrorExpression();
             }
 
-            if (!_scope.TryLookup(name, out var variable))
+            if (!_scope.TryLookupVariable(name, out var variable))
             {
                 _diagnostics.ReportUndefinedName(syntax.IdentifierToken.Span, name);
                 return new BoundErrorExpression();
@@ -223,7 +233,7 @@ namespace Minsk.CodeAnalysis.Binding
             var name = syntax.IdentifierToken.Text;
             var boundExpression = BindExpression(syntax.Expression);
 
-            if (!_scope.TryLookup(name, out var variable))
+            if (!_scope.TryLookupVariable(name, out var variable))
             {
                 _diagnostics.ReportUndefinedName(syntax.IdentifierToken.Span, name);
                 return boundExpression;
@@ -288,10 +298,7 @@ namespace Minsk.CodeAnalysis.Binding
                 boundArguments.Add(boundArgument);
             }
 
-            var functions = BuiltinFunctions.GetAll();
-
-            var function = functions.SingleOrDefault(f => f.Name == syntax.Identifier.Text);
-            if (function == null)
+            if (!_scope.TryLookupFunction(syntax.Identifier.Text, out var function))
             {
                 _diagnostics.ReportUndefinedFunction(syntax.Identifier.Span, syntax.Identifier.Text);
                 return new BoundErrorExpression();
@@ -324,7 +331,7 @@ namespace Minsk.CodeAnalysis.Binding
             var declare = !identifier.IsMissing;
             var variable = new VariableSymbol(name, isReadOnly, type);
 
-            if (declare && !_scope.TryDeclare(variable))
+            if (declare && !_scope.TryDeclareVariable(variable))
                 _diagnostics.ReportVariableAlreadyDeclared(identifier.Span, name);
 
             return variable;
--- src/Minsk/CodeAnalysis/Binding/BoundScope.cs
+++ src/Minsk/CodeAnalysis/Binding/BoundScope.cs
@@ -6,7 +6,8 @@ namespace Minsk.CodeAnalysis.Binding
 {
     internal sealed class BoundScope
     {
-        private Dictionary<string, VariableSymbol> _variables = new Dictionary<string, VariableSymbol>();
+        private Dictionary<string, VariableSymbol> _variables;
+        private Dictionary<string, FunctionSymbol> _functions;
 
         public BoundScope(BoundScope parent)
         {
@@ -15,8 +16,11 @@ namespace Minsk.CodeAnalysis.Binding
 
         public BoundScope Parent { get; }
 
-        public bool TryDeclare(VariableSymbol variable)
+        public bool TryDeclareVariable(VariableSymbol variable)
         {
+            if (_variables == null)
+                _variables = new Dictionary<string, VariableSymbol>();
+
             if (_variables.ContainsKey(variable.Name))
                 return false;
 
@@ -24,20 +28,58 @@ namespace Minsk.CodeAnalysis.Binding
             return true;
         }
 
-        public bool TryLookup(string name, out VariableSymbol variable)
+        public bool TryLookupVariable(string name, out VariableSymbol variable)
         {
-            if (_variables.TryGetValue(name, out variable))
+            variable = null;
+
+            if (_variables != null && _variables.TryGetValue(name, out variable))
                 return true;
 
             if (Parent == null)
                 return false;
 
-            return Parent.TryLookup(name, out variable);
+            return Parent.TryLookupVariable(name, out variable);
+        }
+
+        public bool TryDeclareFunction(FunctionSymbol function)
+        {
+            if (_functions == null)
+                _functions = new Dictionary<string, FunctionSymbol>();
+
+            if (_functions.ContainsKey(function.Name))
+                return false;
+
+            _functions.Add(function.Name, function);
+            return true;
+        }
+
+        public bool TryLookupFunction(string name, out FunctionSymbol function)
+        {
+            function = null;
+
+            if (_functions != null && _functions.TryGetValue(name, out function))
+                return true;
+
+            if (Parent == null)
+                return false;
+
+            return Parent.TryLookupFunction(name, out function);
         }
 
         public ImmutableArray<VariableSymbol> GetDeclaredVariables()
         {
+            if (_variables == null)
+                return ImmutableArray<VariableSymbol>.Empty;
+
             return _variables.Values.ToImmutableArray();
+        }
+
+        public ImmutableArray<FunctionSymbol> GetDeclaredFunctions()
+        {
+            if (_functions == null)
+                return ImmutableArray<FunctionSymbol>.Empty;
+
+            return _functions.Values.ToImmutableArray();
         }
     }
 }

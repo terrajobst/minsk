9b6cdb9a5b7335fe80ef2ee066426a99af6d23b4
Immo Landwerth <immo@landwerth.net>
2019-05-22 08:27:57 -0700

    Add string == and !=

--- src/Minsk/CodeAnalysis/Binding/BoundBinaryOperator.cs
+++ src/Minsk/CodeAnalysis/Binding/BoundBinaryOperator.cs
@@ -56,6 +56,8 @@ namespace Minsk.CodeAnalysis.Binding
             new BoundBinaryOperator(SyntaxKind.BangEqualsToken, BoundBinaryOperatorKind.NotEquals, TypeSymbol.Bool),
 
             new BoundBinaryOperator(SyntaxKind.PlusToken, BoundBinaryOperatorKind.Addition, TypeSymbol.String),
+            new BoundBinaryOperator(SyntaxKind.EqualsEqualsToken, BoundBinaryOperatorKind.Equals, TypeSymbol.String, TypeSymbol.Bool),
+            new BoundBinaryOperator(SyntaxKind.BangEqualsToken, BoundBinaryOperatorKind.NotEquals, TypeSymbol.String, TypeSymbol.Bool),
         };
 
         public static BoundBinaryOperator Bind(SyntaxKind syntaxKind, TypeSymbol leftType, TypeSymbol rightType)

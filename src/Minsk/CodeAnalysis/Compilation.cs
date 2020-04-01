f3dfd3cd218d474beee1affc1bb88059f7dcaf83
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-04-01 23:46:23 +0200

    Add built-in functions to GetSymbols()

--- src/Minsk/CodeAnalysis/Compilation.cs
+++ src/Minsk/CodeAnalysis/Compilation.cs
@@ -9,6 +9,8 @@ using Minsk.CodeAnalysis.Lowering;
 using Minsk.CodeAnalysis.Symbols;
 using Minsk.CodeAnalysis.Syntax;
 
+using ReflectionBindingFlags = System.Reflection.BindingFlags;
+
 namespace Minsk.CodeAnalysis
 {
     public sealed class Compilation
@@ -52,6 +54,19 @@ namespace Minsk.CodeAnalysis
 
             while (submission != null)
             {
+                const ReflectionBindingFlags bindingFlags = 
+                    ReflectionBindingFlags.Static |
+                    ReflectionBindingFlags.Public |
+                    ReflectionBindingFlags.NonPublic;
+                var builtinFunctions = typeof(BuiltinFunctions)
+                    .GetFields(bindingFlags)
+                    .Where(fi => fi.FieldType == typeof(FunctionSymbol))
+                    .Select(fi => (FunctionSymbol)fi.GetValue(obj: null))
+                    .ToList();
+                foreach (var builtin in builtinFunctions)
+                    if (seenSymbolNames.Add(builtin.Name))
+                        yield return builtin;
+
                 foreach (var function in submission.Functions)
                     if (seenSymbolNames.Add(function.Name))
                         yield return function;

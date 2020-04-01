68bf8e8bb3587866fc06e0e33f07ff9648df2307
Immo Landwerth <immo@landwerth.net>
2020-04-01 09:19:30 -0700

    Colorize help

--- src/msi/Repl.cs
+++ src/msi/Repl.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Minsk.IO;
 
 namespace Minsk
 {
@@ -532,7 +533,14 @@ namespace Minsk
             foreach (var metaCommand in _metaCommands.OrderBy(mc => mc.Name))
             {
                 var paddedName = metaCommand.Name.PadRight(maxNameLength);
-                Console.WriteLine($"#{paddedName}  {metaCommand.Description}");
+
+                Console.Out.WritePunctuation("#");
+                Console.Out.WriteIdentifier(paddedName);
+                Console.Out.WriteSpace();
+                Console.Out.WriteSpace();
+                Console.Out.WriteSpace();
+                Console.Out.WritePunctuation(metaCommand.Description);
+                Console.Out.WriteLine();
             }
         }
     }

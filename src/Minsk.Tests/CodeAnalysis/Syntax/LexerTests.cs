41148d5c71b9000eecb9ae2762c210371d510418
Stefan Hoffmann <stefan991@gmail.com>
2020-04-05 15:45:05 +0200

    Add Tests for SyntaxTrivia to LexterTests

--- src/Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ src/Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -87,9 +87,122 @@ namespace Minsk.Tests.CodeAnalysis.Syntax
             Assert.Equal(t2Text, tokens[1].Text);
         }
 
+        [Fact]
+        public void Lexer_Covers_AllTrivia()
+        {
+            var triviaKinds = Enum.GetValues(typeof(SyntaxKind))
+                                  .Cast<SyntaxKind>()
+                                  .Where(k => k.ToString().EndsWith("Trivia"));
+
+            var testedTriviaKinds = GetTrivia().Select(t => t.kind);
+
+            var untestedTriviaKinds = new SortedSet<SyntaxKind>(triviaKinds);
+            untestedTriviaKinds.ExceptWith(testedTriviaKinds);
+
+            Assert.Empty(untestedTriviaKinds);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTriviaData))]
+        public void Lexer_Lexes_TrailingTrivia(SyntaxKind kind, string text)
+        {
+            var tokens = SyntaxTree.ParseTokens("tokenWithTrailingTrivia" + text);
+
+            var token = Assert.Single(tokens);
+            Assert.Equal(SyntaxKind.IdentifierToken, token.Kind);
+            Assert.Empty(token.LeadingTrivia);
+            var trivia = Assert.Single(token.TrailingTrivia);
+            Assert.Equal(kind, trivia.Kind);
+            Assert.Equal(text, trivia.Text);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTriviaData))]
+        public void Lexer_Lexes_LeadingTrivia(SyntaxKind kind, string text)
+        {
+            if (kind == SyntaxKind.SingleLineCommentTrivia)
+                return;
+
+            var tokens = SyntaxTree.ParseTokens(text + "tokenWithLeadingTrivia");
+
+            var token = Assert.Single(tokens);
+            Assert.Equal(SyntaxKind.IdentifierToken, token.Kind);
+            var trivia = Assert.Single(token.LeadingTrivia);
+            Assert.Equal(kind, trivia.Kind);
+            Assert.Equal(text, trivia.Text);
+            Assert.Empty(token.TrailingTrivia);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTriviaData))]
+        public void Lexer_Lexes_EndOfFileLeadingTrivia(SyntaxKind kind, string text)
+        {
+            var tokens = SyntaxTree.ParseTokens(text, includeEndOfFile: true);
+
+            var token = Assert.Single(tokens);
+            Assert.Equal(SyntaxKind.EndOfFileToken, token.Kind);
+            var trivia = Assert.Single(token.LeadingTrivia);
+            Assert.Empty(token.TrailingTrivia);
+            Assert.Equal(kind, trivia.Kind);
+            Assert.Equal(text, trivia.Text);
+            Assert.Empty(token.TrailingTrivia);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTriviaLineEndingPairsData))]
+        public void Lexer_Lexes_LeadingTriviaFromPreviousLine(SyntaxKind kind, string text, string eolText)
+        {
+            var tokens = SyntaxTree.ParseTokens(text + eolText + "tokenWithLeadingTrivia");
+
+            var token = Assert.Single(tokens);
+            Assert.Equal(SyntaxKind.IdentifierToken, token.Kind);
+            Assert.Collection(token.LeadingTrivia,
+                trivia =>
+                {
+                    Assert.Equal(kind, trivia.Kind);
+                    Assert.Equal(text, trivia.Text);
+                },
+                eolTrivia =>
+                {
+                    Assert.Equal(SyntaxKind.EndOfLineTrivia, eolTrivia.Kind);
+                    Assert.Equal(eolText, eolTrivia.Text);
+                }
+            );
+            Assert.Empty(token.TrailingTrivia);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTriviaLineEndingPairsData))]
+        public void Lexer_Lexes_EndOfFileLeadingTriviaFromPreviousLine(SyntaxKind kind, string text, string eolText)
+        {
+            var tokens = SyntaxTree.ParseTokens(text + eolText, includeEndOfFile: true);
+
+            var token = Assert.Single(tokens);
+            Assert.Equal(SyntaxKind.EndOfFileToken, token.Kind);
+            Assert.Collection(token.LeadingTrivia,
+                trivia =>
+                {
+                    Assert.Equal(kind, trivia.Kind);
+                    Assert.Equal(text, trivia.Text);
+                },
+                eolTrivia =>
+                {
+                    Assert.Equal(SyntaxKind.EndOfLineTrivia, eolTrivia.Kind);
+                    Assert.Equal(eolText, eolTrivia.Text);
+                }
+            );
+            Assert.Empty(token.TrailingTrivia);
+        }
+
         public static IEnumerable<object[]> GetTokensData()
         {
             foreach (var t in GetTokens())
+                yield return new object[] { t.kind, t.text };
+        }
+
+        public static IEnumerable<object[]> GetTriviaData()
+        {
+            foreach (var t in GetTrivia())
                 yield return new object[] { t.kind, t.text };
         }
 
@@ -103,6 +216,12 @@ namespace Minsk.Tests.CodeAnalysis.Syntax
         {
             foreach (var t in GetTokenPairsWithSeparator())
                 yield return new object[] { t.t1Kind, t.t1Text, t.separatorKind, t.separatorText, t.t2Kind, t.t2Text };
+        }
+
+        public static IEnumerable<object[]> GetTriviaLineEndingPairsData()
+        {
+            foreach (var t in GetTriviaLineEndingPairs())
+                yield return new object[] { t.kind, t.text, t.eolText};
         }
 
         private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
@@ -126,16 +245,38 @@ namespace Minsk.Tests.CodeAnalysis.Syntax
             return fixedTokens.Concat(dynamicTokens);
         }
 
-        private static IEnumerable<(SyntaxKind kind, string text)> GetSeparators()
+        private static IEnumerable<(SyntaxKind kind, string text)> GetLineEndings()
         {
             return new[]
             {
-                (SyntaxKind.WhitespaceTrivia, " "),
-                (SyntaxKind.WhitespaceTrivia, "  "),
                 (SyntaxKind.EndOfLineTrivia, "\r"),
                 (SyntaxKind.EndOfLineTrivia, "\n"),
                 (SyntaxKind.EndOfLineTrivia, "\r\n")
             };
+        }
+
+        private static IEnumerable<(SyntaxKind kind, string text)> GetSeparators()
+        {
+            var seperators = new[]
+            {
+                (SyntaxKind.WhitespaceTrivia, " "),
+                (SyntaxKind.WhitespaceTrivia, "  "),
+            };
+
+            return seperators.Concat(GetLineEndings());
+        }
+
+        private static IEnumerable<(SyntaxKind kind, string text)> GetTrivia()
+        {
+            var trivia = new[]
+            {
+                (SyntaxKind.SingleLineCommentTrivia, "//"),
+                (SyntaxKind.SingleLineCommentTrivia, "// "),
+                (SyntaxKind.SingleLineCommentTrivia, "//a"),
+                (SyntaxKind.SingleLineCommentTrivia, "///"),
+            };
+
+            return trivia.Concat(GetSeparators());
         }
 
         private static bool RequiresSeparator(SyntaxKind t1Kind, SyntaxKind t2Kind)
@@ -228,6 +369,18 @@ namespace Minsk.Tests.CodeAnalysis.Syntax
                         foreach (var s in GetSeparators())
                             yield return (t1.kind, t1.text, s.kind, s.text, t2.kind, t2.text);
                     }
+                }
+            }
+        }
+
+        private static IEnumerable<(SyntaxKind kind, string text, string eolText)> GetTriviaLineEndingPairs()
+        {
+            foreach (var trivia in GetTrivia())
+            {
+                foreach (var lineEnding in GetLineEndings())
+                {
+                    if (trivia.kind != SyntaxKind.EndOfLineTrivia)
+                        yield return (trivia.kind, trivia.text, lineEnding.text);
                 }
             }
         }

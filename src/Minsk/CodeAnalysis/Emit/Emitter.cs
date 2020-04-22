e6d7c4cc1f67ef63b4b6357aa8219858e2dd18a8
Immo Landwerth <immo@landwerth.net>
2020-04-22 09:19:00 -0700

    Emit string concat

--- src/Minsk/CodeAnalysis/Emit/Emitter.cs
+++ src/Minsk/CodeAnalysis/Emit/Emitter.cs
@@ -17,6 +17,7 @@ namespace Minsk.CodeAnalysis.Emit
         private readonly Dictionary<TypeSymbol, TypeReference> _knownTypes;
         private readonly MethodReference _consoleReadLineReference;
         private readonly MethodReference _consoleWriteLineReference;
+        private readonly MethodReference _stringConcatReference;
         private readonly AssemblyDefinition _assemblyDefinition;
         private readonly Dictionary<FunctionSymbol, MethodDefinition> _methods = new Dictionary<FunctionSymbol, MethodDefinition>();
         private readonly Dictionary<VariableSymbol, VariableDefinition> _locals = new Dictionary<VariableSymbol, VariableDefinition>();
@@ -132,6 +133,7 @@ namespace Minsk.CodeAnalysis.Emit
 
             _consoleReadLineReference = ResolveMethod("System.Console", "ReadLine", Array.Empty<string>());
             _consoleWriteLineReference = ResolveMethod("System.Console", "WriteLine", new [] { "System.String" });
+            _stringConcatReference = ResolveMethod("System.String", "Concat", new [] { "System.String", "System.String" });
         }
 
         public static ImmutableArray<Diagnostic> Emit(BoundProgram program, string moduleName, string[] references, string outputPath)
@@ -329,7 +331,24 @@ namespace Minsk.CodeAnalysis.Emit
 
         private void EmitBinaryExpression(ILProcessor ilProcessor, BoundBinaryExpression node)
         {
-            throw new NotImplementedException();
+            if (node.Op.Kind == BoundBinaryOperatorKind.Addition)
+            {
+                if (node.Left.Type == TypeSymbol.String &&
+                    node.Right.Type == TypeSymbol.String)
+                {
+                    EmitExpression(ilProcessor, node.Left);
+                    EmitExpression(ilProcessor, node.Right);
+                    ilProcessor.Emit(OpCodes.Call, _stringConcatReference);
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
 
         private void EmitCallExpression(ILProcessor ilProcessor, BoundCallExpression node)

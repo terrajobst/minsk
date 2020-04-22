d31fc015f02ed446fdcd068b74da01170f22642c
Immo Landwerth <immo@landwerth.net>
2020-04-22 09:43:38 -0700

    Add ParameterSymbol.Ordinal

--- src/Minsk/CodeAnalysis/Symbols/BuiltinFunctions.cs
+++ src/Minsk/CodeAnalysis/Symbols/BuiltinFunctions.cs
@@ -8,9 +8,9 @@ namespace Minsk.CodeAnalysis.Symbols
 {
     internal static class BuiltinFunctions
     {
-        public static readonly FunctionSymbol Print = new FunctionSymbol("print", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String)), TypeSymbol.Void);
+        public static readonly FunctionSymbol Print = new FunctionSymbol("print", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String, 0)), TypeSymbol.Void);
         public static readonly FunctionSymbol Input = new FunctionSymbol("input", ImmutableArray<ParameterSymbol>.Empty, TypeSymbol.String);
-        public static readonly FunctionSymbol Rnd = new FunctionSymbol("rnd", ImmutableArray.Create(new ParameterSymbol("max", TypeSymbol.Int)), TypeSymbol.Int);
+        public static readonly FunctionSymbol Rnd = new FunctionSymbol("rnd", ImmutableArray.Create(new ParameterSymbol("max", TypeSymbol.Int, 0)), TypeSymbol.Int);
 
         internal static IEnumerable<FunctionSymbol> GetAll()
             => typeof(BuiltinFunctions).GetFields(BindingFlags.Public | BindingFlags.Static)
--- src/Minsk/CodeAnalysis/Symbols/ParameterSymbol.cs
+++ src/Minsk/CodeAnalysis/Symbols/ParameterSymbol.cs
@@ -2,11 +2,13 @@ namespace Minsk.CodeAnalysis.Symbols
 {
     public sealed class ParameterSymbol : LocalVariableSymbol
     {
-        public ParameterSymbol(string name, TypeSymbol type)
+        public ParameterSymbol(string name, TypeSymbol type, int ordinal)
             : base(name, isReadOnly: true, type)
         {
+            Ordinal = ordinal;
         }
 
         public override SymbolKind Kind => SymbolKind.Parameter;
+        public int Ordinal { get; }
     }
 }
